1572acf63adad20f7c351d4e7488cdb99ea4ddc2
mehrozasif <mehroz.rj@gmail.com>
2021-11-27 22:52:46 +0500

    auth,redux,library

--- src/components/smallcomponents/Header.js
+++ src/components/smallcomponents/Header.js
@@ -7,8 +7,8 @@ function Header(props) {
   const navigate = useNavigate();
   const state = useSelector((state) => state);
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
+    <div className="shade">
+      <Navbar bg="lighter" className="text-white" expand="lg">
         <Container>
           <Navbar.Brand>Dashboard</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -20,7 +20,7 @@ function Header(props) {
               <Nav className="grey mx-2"> {state?.user}</Nav>
               <Nav className="">
                 <button
-                  class="btn border-0 mx-1 br-8 px-3 bg-danger text-white"
+                  class="btn border-0 mx-1 br-8 px-3 btn-danger text-white"
                   onClick={() => {
                     dispatch({
                       type: "LOG_OUT",
